61b1bf77033ef17ac6908af6d813250696a31ad3
denregr <den.regr@gmail.com>
2020-04-03 04:42:50 +1000

    1/20 task complite

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-print("Hello Home!")
-def test():
+#1_в_ожидании нового года
+def newYearTimeCount(m,n):
+    n = 1440 - (n + m * 60)
+    m, n = n // 60, n % 60
+    return m, n
+# Куб со спицами
+def cubes():
     pass
+
+#отгадай слово
+def world():
+    pass
+
+print(newYearTimeCount(int(input()),int(input())))
